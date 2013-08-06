8d67df3d378c29c026553fd23aeb013b80b2b494
Rohit.Ranjan <91.rohit@gmail.com>
2013-08-06 20:09:52 +0530

    added splash screen now

---
+++ WifiComm/src/com/wificomm/splashscreen/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.wificomm.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.wificomm.MainActivity;
+import com.example.wificomm.R;
+import com.wificomm.constants.Constants;
+
+public class SplashScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		 requestWindowFeature(Window.FEATURE_NO_TITLE);
+	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	        
+	        setContentView(R.layout.splashscreen);
+	        new Handler().postDelayed(new Runnable(){
+	            @Override
+	            public void run() {
+	                /* Create an Intent that will start the Menu-Activity. */
+	                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+	                SplashScreen.this.startActivity(mainIntent);
+	                SplashScreen.this.finish();
+	            }
+	        }, Constants.SPLASH_DISPLAY_LENGHT);
+	       
+		
+	}
+	
+
+}
