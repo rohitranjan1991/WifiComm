c487c2231612f157bdac1a28476d295a4552f483
Rohit.Ranjan <91.rohit@gmail.com>
2013-08-06 16:09:56 +0530

    added few files

---
+++ WifiComm/src/com/example/constants/Constants.java
@@ -0,0 +1,18 @@
+package com.example.constants;
+
+public class Constants {
+
+	public static int callTimeoutTime=10000;
+	
+	
+	/*public static Constants instance=null;
+	
+	public static Constants getInstance()
+	{
+		if(instance==null)
+			instance=new Constants();
+		return instance;
+	}
+	*/
+	
+}
