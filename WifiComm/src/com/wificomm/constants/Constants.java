f61e1e630115e713631f7a127500d4cdc8bb26e1
Rohit.Ranjan <91.rohit@gmail.com>
2013-08-06 20:08:24 +0530

    added SplashScreen for 10 secs

--- WifiComm/src/com/wificomm/constants/Constants.java
+++ WifiComm/src/com/wificomm/constants/Constants.java
@@ -3,7 +3,7 @@ package com.wificomm.constants;
 public class Constants {
 
 	public static int callTimeoutTime=10000;
-	
+	public static final int SPLASH_DISPLAY_LENGHT = 5000;
 	
 	/*public static Constants instance=null;
 	
